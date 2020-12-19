3711d3a2b2d63902a3f89fdaa8a439fa1a11bbbb
Dean <deancs1997@gmail.com>
2020-12-19 18:39:53 -0500

    refactored and added icon to make sure that we have the three tabs

--- Front End/Android/app/src/main/java/com/theelite/portal/ui/settings/SettingsViewModel.kt
+++ Front End/Android/app/src/main/java/com/theelite/portal/ui/settings/SettingsViewModel.kt
@@ -1,10 +1,10 @@
-package com.theelite.portal.ui.dashboard
+package com.theelite.portal.ui.settings
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class SettingsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
