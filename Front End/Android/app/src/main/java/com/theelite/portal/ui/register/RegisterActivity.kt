ccc81af86957210f1236d966196100feecd84a3e
Dean <deancs1997@gmail.com>
2020-12-21 16:03:45 -0500

    Added view for registering an account

--- Front End/Android/app/src/main/java/com/theelite/portal/ui/register/RegisterActivity.kt
+++ Front End/Android/app/src/main/java/com/theelite/portal/ui/register/RegisterActivity.kt
@@ -1,4 +1,18 @@
 package com.theelite.portal.ui.register
 
-class RegisterActivity {
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.theelite.portal.R
+
+class RegisterActivity : AppCompatActivity() {
+    lateinit var registerButton: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        registerButton = findViewById(R.id.registerButton)
+        registerButton.setOnClickListener {
+            println("Clicked on Button!")
+        }
+    }
 }
