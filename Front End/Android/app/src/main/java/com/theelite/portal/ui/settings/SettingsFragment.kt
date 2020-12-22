b6623937fcd40661977ecb4aa771756b1d65dfd8
Dean <deancs1997@gmail.com>
2020-12-22 13:23:58 -0500

    Have a working recycler view for settings view :o
    Can now display options for the settings

--- Front End/Android/app/src/main/java/com/theelite/portal/ui/settings/SettingsFragment.kt
+++ Front End/Android/app/src/main/java/com/theelite/portal/ui/settings/SettingsFragment.kt
@@ -4,27 +4,46 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.theelite.portal.R
 
 class SettingsFragment : Fragment() {
 
     private lateinit var settingsViewModel: SettingsViewModel
+    private lateinit var settingsRecyclerView: RecyclerView
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        settingsViewModel = ViewModelProviders.of(this).get(SettingsViewModel::class.java)
+//        settingsViewModel = ViewModelProviders.of(this).get(SettingsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_settings, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        settingsRecyclerView = root.findViewById(R.id.settingsRecyclerView)
+        settingsRecyclerView.layoutManager =
+            LinearLayoutManager(this.context, RecyclerView.VERTICAL, false)
+        val options = ArrayList<String>()
+        options.add("Account")
+        options.add("Notifications")
+        options.add("Help")
+        options.add("Log out")
+        val settingsAdapter = SettingsAdapter(options)
+        settingsRecyclerView.adapter = settingsAdapter
+        //        val textView: TextView = root.findViewById(R.id.text_dashboard)
+//        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
         return root
     }
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        settingsRecyclerView = findViewById(R.id.settingsRecyclerView)
+//    }
 }
--- Front End/Android/app/src/main/java/com/theelite/portal/ui/settings/SettingsViewModel.kt
+++ Front End/Android/app/src/main/java/com/theelite/portal/ui/settings/SettingsViewModel.kt
@@ -3,9 +3,9 @@ package com.theelite.portal.ui.settings
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.RecyclerView
 
 class SettingsViewModel : ViewModel() {
-
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
     }
