70b6c2acce3419628b3b65b92ddee75badedc6b9
Dean <deancs1997@gmail.com>
2021-01-01 21:39:45 -0500

    Added classes for the account settings page and updated the icons

---
+++ Front End/Android/app/src/main/java/com/theelite/portal/ui/accountSettings/AccountSettings.kt
@@ -0,0 +1,14 @@
+package com.theelite.portal.ui.accountSettings
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.theelite.portal.R
+
+class AccountSettings : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account)
+    }
+
+}
