61942f33136eb58c9fa2968e73af628dbe670722
Dean <deancs1997@gmail.com>
2021-01-02 21:57:01 -0500

    updated the dependencies
    also added support for pull to refresh in recent notifications fragment

--- Front End/Android/app/src/main/java/com/theelite/portal/ui/notifications/NotificationsFragment.kt
+++ Front End/Android/app/src/main/java/com/theelite/portal/ui/notifications/NotificationsFragment.kt
@@ -1,39 +1,55 @@
 package com.theelite.portal.ui.notifications
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.theelite.portal.R
 
 class NotificationsFragment : Fragment() {
 
-    //    private lateinit var notificationsViewModel: NotificationsViewModel
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
     private lateinit var recentNotificationRecyclerView: RecyclerView
+    private lateinit var root: View
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-//        notificationsViewModel = ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-//        val textView: TextView = root.findViewById(R.id.text_notifications)
-//        notificationsViewModel.text.observe(viewLifecycleOwner, Observer { textView.text = it })
+    ): View {
+
+        root = inflater.inflate(R.layout.fragment_notifications, container, false)
+        setUpRecyclerView()
+        setUpRefreshLayout()
+        return root
+    }
+
+
+    //TODO: Allow user to fetch new notifications
+    private fun setUpRefreshLayout() {
+        swipeRefreshLayout = root.findViewById(R.id.swipeRefreshRecentNotifications)
+        swipeRefreshLayout.setOnRefreshListener {
+                println("Recent Notifications -- Starting to Refresh!!")
+                val handler = Handler()
+                handler.postDelayed(Runnable {
+                    if (swipeRefreshLayout.isRefreshing) swipeRefreshLayout.isRefreshing = false
+                }, 1000)
+        }
+    }
+
+
+    private fun setUpRecyclerView() {
         recentNotificationRecyclerView = root.findViewById(R.id.recentNotificationsRecyclerView)
         recentNotificationRecyclerView.layoutManager =
             LinearLayoutManager(this.context, RecyclerView.VERTICAL, false)
         val recentNotificationsAdapter = RecentNotificationsAdapter(getNotifications())
         recentNotificationRecyclerView.adapter = recentNotificationsAdapter
-        return root
     }
-
 
     private fun getNotifications(): ArrayList<String> {
         val notifications = ArrayList<String>()
