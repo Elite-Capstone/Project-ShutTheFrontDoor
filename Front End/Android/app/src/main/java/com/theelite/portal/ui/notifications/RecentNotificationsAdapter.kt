f7635d5c148d5084ddd731d21438c518c7225d5e
Dean <deancs1997@gmail.com>
2021-01-06 18:02:16 -0500

    added second cell view for notifications
    now display the alternative view for each even notification

--- Front End/Android/app/src/main/java/com/theelite/portal/ui/notifications/RecentNotificationsAdapter.kt
+++ Front End/Android/app/src/main/java/com/theelite/portal/ui/notifications/RecentNotificationsAdapter.kt
@@ -14,22 +14,46 @@ class RecentNotificationsAdapter(private val dataSet: ArrayList<String>) :
 
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
         val titleTextView: TextView
         val descriptionTextView: TextView
         val dateTextView: TextView
 
         init {
-            titleTextView = view.findViewById(R.id.recentNotificationsNoActionsTitleLabel)
-            descriptionTextView =
-                view.findViewById(R.id.recentNotificationsNoActionsDescriptionLabel)
-            dateTextView = view.findViewById(R.id.recentNotificationsNoActionsDateLabel)
+            if (view.getTag(R.layout.recent_notifications_w_actions) == "recent") {
+                titleTextView = view.findViewById(R.id.recentNotificationsWActionsTitleLabel)
+                descriptionTextView =
+                    view.findViewById(R.id.recentNotificationsWActionsDescriptionLabel)
+                dateTextView = view.findViewById(R.id.recentNotificationsWActionsDateLabel)
+            } else {
+                titleTextView = view.findViewById(R.id.recentNotificationsNoActionsTitleLabel)
+                descriptionTextView =
+                    view.findViewById(R.id.recentNotificationsNoActionsDescriptionLabel)
+                dateTextView = view.findViewById(R.id.recentNotificationsNoActionsDateLabel)
+            }
         }
 
     }
 
+    override fun getItemViewType(position: Int): Int {
+        return position
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.recent_notifications_no_actions, parent, false)
+        var view: View
+
+        if (viewType % 2 == 0) {
+            println("View type $viewType is even")
+            view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.recent_notifications_no_actions, parent, false)
+            view.setTag(R.layout.recent_notifications_no_actions, "old")
+
+        } else {
+            println("View type $viewType is odd")
+            view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.recent_notifications_w_actions, parent, false)
+            view.setTag(R.layout.recent_notifications_w_actions, "recent")
+        }
         return ViewHolder(view)
     }
 
