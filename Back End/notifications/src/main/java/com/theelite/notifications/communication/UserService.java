2c3e04c212510bc8efaacf3e98a7c77ea356dac3
Dean <deancs1997@gmail.com>
2021-02-21 16:08:35 -0500

    implementation for notif ms to talk to user ms

--- Back End/notifications/src/main/java/com/theelite/notifications/communication/UserService.java
+++ Back End/notifications/src/main/java/com/theelite/notifications/communication/UserService.java
@@ -1,2 +1,12 @@
-package com.theelite.notifications.communication;public interface UserService {
+package com.theelite.notifications.communication;
+
+import com.theelite.notifications.model.User;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+
+public interface UserService {
+
+    @GET("/")
+    Call<User> getUserWithId(@Body User user);
 }
