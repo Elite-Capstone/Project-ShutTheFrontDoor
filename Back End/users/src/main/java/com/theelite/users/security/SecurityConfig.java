75c5f8130f765ebde91fc56646068221ffc06bb7
Dean <deancs1997@gmail.com>
2021-02-21 12:57:37 -0500

    added another line to remove the error with https requests

--- Back End/users/src/main/java/com/theelite/users/security/SecurityConfig.java
+++ Back End/users/src/main/java/com/theelite/users/security/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("*").permitAll();
+        http.headers().httpStrictTransportSecurity().disable();
         http.csrf().disable();
     }
 
