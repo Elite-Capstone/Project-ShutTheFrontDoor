fd22d0c1ff4c58f67522c7a47d3978f534549f43
Dean <deancs1997@gmail.com>
2021-02-24 14:39:30 -0500

    notifications should appear in order buttons appear if date <20
    also refactored some code

--- Front End/Android/app/src/main/java/com/theelite/portal/Objects/User.kt
+++ Front End/Android/app/src/main/java/com/theelite/portal/Objects/User.kt
@@ -3,4 +3,4 @@ package com.theelite.portal.Objects
 import android.provider.ContactsContract
 import java.util.*
 
-data class User(val email: String, val firstName : String, val lastName: String, val password: String?,val accountId : UUID?, val role: UserRole?)
+data class User(var email: String, val firstName : String?, val lastName: String?, val password: String?, val accountId : UUID?, val role: UserRole?)
