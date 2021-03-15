8a9fdccbf1912de177f85f69fe495f562b86e9e5
Olivier Bertrand-Vachet <olivier.b.vachet@gmail.com>
2021-03-15 18:16:03 -0400

    pub/sub

--- Hardware/ESP/opencd_videostream/openCD_pub.py
+++ Hardware/ESP/opencd_videostream/openCD_pub.py
@@ -48,25 +48,3 @@ def pubVideo(config):
     print("End Time: ", datetime.datetime.now())
 
 
-#sub with python CvZmq.py sub --subport=5555
-
-def subVideo(config):
-    context = zmq.Context()
-    footage_socket = context.socket(zmq.SUB)
-    port = 5555
-    bind_address = "tcp://192.168.1.19:{}".format(port)  # 'tcp://*:5555'
-    print("Subscribe Video at ", bind_address)
-    footage_socket.bind(bind_address)
-    footage_socket.setsockopt_string(zmq.SUBSCRIBE, unicode(''))
-    while True:
-        try:
-            frame = footage_socket.recv_string()
-            img = base64.b64decode(frame)
-            npimg = np.fromstring(img, dtype=np.uint8)
-            source = cv2.imdecode(npimg, 1)
-            cv2.imshow("image", source)
-            cv2.waitKey(1)
-        except KeyboardInterrupt:
-            cv2.destroyAllWindows()
-            print("\n\nBye bye\n")
-            break
