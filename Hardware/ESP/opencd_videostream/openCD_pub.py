49b598d46bf6c1685253e010e731085dccabdcd9
Benjamin <benjilan34@hotmail.com>
2021-03-16 11:27:03 -0400

    Trying stuff with Python Pub-Sub

--- Hardware/ESP/opencd_videostream/openCD_pub.py
+++ Hardware/ESP/opencd_videostream/openCD_pub.py
@@ -12,27 +12,29 @@ from imutils.video import FPS
 
 ### run with python openCD.py pub --192.168.1.19/stream --pubport=80
 
-
-
 context = zmq.Context()
 footage_socket = context.socket(zmq.PUB)
 # 'tcp://localhost:5555'
-ip = "192.168.1.19"
+#ip = "192.168.1.19"
+ip = 'localhost'
 port = 5555
 target_address = "tcp://{}:{}".format(ip, port)
 print("Publish Video to ", target_address)
 footage_socket.connect(target_address)
 camera = cv2.VideoCapture('http://192.168.1.19:80/stream')
-camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+#camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+#camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 print("Start Time: ", datetime.datetime.now())
 fps = FPS().start()
 while True:
         try:
-            (grabbed, frame) = camera.read()
-            frame = cv2.resize(frame, (640, 480)) 
+            (grabbed, frame) = camera.read()  # grab the current frame
+            #frame = cv2.resize(frame, (640, 480))  # resize the frame
             encoded, buffer = cv2.imencode('.jpg', frame)
-            footage_socket.send_string(base64.b64encode(buffer))
+            #enc = base64.b64encode(buffer)
+            #string = str(enc)
+            string = str(buffer)
+            footage_socket.send_string(string, encoding='utf-8')
             # Update the FPS counter
             fps.update()
             cv2.waitKey(1)
