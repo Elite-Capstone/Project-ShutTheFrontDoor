142baa57b20902d268bd2c6e3e7b803da8965fbf
RaduP13 <46818067+RaduP13@users.noreply.github.com>
2021-03-24 23:57:47 -0400

    Origin/frontend/downloadvideo (#181)
    * media fetch
    
    * video download implementation
    
    completed the implementation of video download

---
+++ Front End/Android/app/src/main/java/com/theelite/portal/request/MediaService.kt
@@ -0,0 +1,16 @@
+package com.theelite.portal.request
+
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface MediaService {
+
+    @GET("file/list/{email}/{token}")
+    fun getFileNames(@Path("email") email: String, @Path("token") token: String) : Call<List<String>>
+
+    @GET("file/download/{filename}")
+    fun downloadFile(@Path("filename") filename: String ): Call<ResponseBody>
+
+}
--- Front End/Android/app/src/main/java/com/theelite/portal/ui/media/MediaFragment.kt
+++ Front End/Android/app/src/main/java/com/theelite/portal/ui/media/MediaFragment.kt
@@ -1,25 +1,44 @@
 package com.theelite.portal.ui.media
 
+import android.content.Intent
+import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.theelite.portal.Objects.Notification
 import com.theelite.portal.R
+import com.theelite.portal.request.MediaService
+import com.theelite.portal.request.NotificationService
+import com.theelite.portal.request.RetroFit
+import com.theelite.portal.ui.ClickListener
 import com.theelite.portal.ui.adapters.VideoDownloadAdapter
+import com.theelite.portal.ui.login.LoginActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
-class MediaFragment : Fragment(){
+class MediaFragment : Fragment() , ClickListener {
 
     private lateinit var root: View
     private lateinit var videoAdapter:VideoDownloadAdapter
     private lateinit var nameList:MutableList<String>
     private lateinit var videoRecyclerView: RecyclerView
 
+    private lateinit var newNames:List<String>
+
     private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+
+    private var email: String? = null
+    private var token: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,22 +50,71 @@ class MediaFragment : Fragment(){
         videoRecyclerView = root.findViewById(R.id.videoRecyclerview)
         videoRecyclerView.layoutManager =
             LinearLayoutManager(this.context, RecyclerView.VERTICAL, false)
-        //populate names list
+        setUpRefreshLayout()
+        loadState()
         nameList = mutableListOf()
-        nameList.add("vid1")
-        nameList.add("vid2")
-        nameList.add("vid3")
+        getFileNames()
         videoAdapter = VideoDownloadAdapter(nameList,this)
         videoRecyclerView.adapter = videoAdapter
         return root
 
     }
 
+    override fun onItemClicked(name: String) {
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(name)
+        requireActivity().startActivity(i)
+    }
+
     private fun setUpRefreshLayout() {
-        swipeRefreshLayout = root.findViewById(R.id.swipeRefreshRecentNotifications)
+        swipeRefreshLayout = root.findViewById(R.id.swipeRefreshMedia)
         swipeRefreshLayout.setOnRefreshListener {
-            //get new names
+            getFileNames()
         }
     }
 
+    private fun getFileNames(){
+        val retrofit = RetroFit.get(getString(R.string.url))
+        val mediaService: MediaService = retrofit.create(MediaService::class.java)
+
+
+        val call = mediaService.getFileNames(
+                "$email",
+                "$token"
+        )
+
+        call.enqueue(object : Callback<List<String>> {
+            override fun onResponse(
+                    call: Call<List<String>>,
+                    response: Response<List<String>>
+            ) {
+                println(response.body()!!.toString())
+                if (response.isSuccessful) {
+                    newNames = findRedundant((response.body() as MutableList<String>?)!!)
+                    nameList.addAll(newNames)
+                    videoAdapter.notifyDataSetChanged()
+                }
+                if (swipeRefreshLayout.isRefreshing) swipeRefreshLayout.isRefreshing = false
+            }
+
+            override fun onFailure(call: Call<List<String>>, t: Throwable) {
+                println("On failure: "+ t.message)
+                Toast.makeText(activity, "${t.message}", Toast.LENGTH_SHORT).show()
+                if (swipeRefreshLayout.isRefreshing) swipeRefreshLayout.isRefreshing = false
+            }
+
+        })
+
+    }
+
+    private fun loadState(){
+        val sharedPreferences: SharedPreferences = this.requireActivity().getSharedPreferences(LoginActivity.SHARED_PREFS, AppCompatActivity.MODE_PRIVATE)
+        email = sharedPreferences.getString(LoginActivity.EMAIL,null)
+        token = sharedPreferences.getString(LoginActivity.TOKEN,null)
+    }
+
+    private fun findRedundant(allString:MutableList<String>):List<String>{
+        allString.removeAll(nameList)
+        return allString
+    }
 }
