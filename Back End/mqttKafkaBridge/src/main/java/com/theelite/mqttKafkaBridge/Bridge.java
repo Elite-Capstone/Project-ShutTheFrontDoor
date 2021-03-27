c9f1cbc869c2bad35d9e8b2cab01dae2f8e2efca
Dean <deancs1997@gmail.com>
2021-03-26 20:33:02 -0400

    Removed Retrofit + ...
    Fixed Logger;
    Now The bridge gets the topics from kafka directly;
    Also fixed a bug when creating new notification objects

--- Back End/mqttKafkaBridge/src/main/java/com/theelite/mqttKafkaBridge/Bridge.java
+++ Back End/mqttKafkaBridge/src/main/java/com/theelite/mqttKafkaBridge/Bridge.java
@@ -1,59 +1,54 @@
 package com.theelite.mqttKafkaBridge;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.theelite.mqttKafkaBridge.communication.NotificationService;
 import com.theelite.mqttKafkaBridge.model.Notification;
 
 
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.KafkaAdminClient;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 import org.eclipse.paho.client.mqttv3.*;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Properties;
 
 @Component
 public class Bridge implements MqttCallback {
     public final int fiveMinutes = 300000;
-    private final Logger logger;
+    public final int oneMinute = fiveMinutes / 5;
+    private Logger logger;
     private final String mqttServerUri;
     private final String mqttClientId = "MqttKafkaBridge";
     private final String brokerUri;
-    public static NotificationService notificationService;
     public static MqttAsyncClient mqtt;
+    private static AdminClient kafkaAdmin;
     public static Producer<String, Notification> kafkaProducer;
 
 
     public Bridge(Environment environment) {
         this.brokerUri = environment.getProperty("spring.kafka.producer.bootstrap-servers");
         this.mqttServerUri = environment.getProperty("mqtt.servers");
-        this.logger = Logger.getLogger(this.getClass().getName());
-        setUpRetroObject(environment.getProperty("notif.url"));
+        setUpLogger();
         connect();
         Thread periodicThread = new Thread(periodicallyGetKafkaTopics);
         periodicThread.start();
     }
 
-    public void setUpRetroObject(String url) {
-        Gson gson = new GsonBuilder()
-                .setLenient()
-                .create();
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(url)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build();
-        notificationService = retrofit.create(NotificationService.class);
+    private void setUpLogger() {
+        this.logger = Logger.getLogger(this.getClass().getName());
+        ConsoleAppender consoleAppender = new ConsoleAppender();
+        consoleAppender.setTarget(ConsoleAppender.SYSTEM_OUT);
+        consoleAppender.activateOptions();
+        consoleAppender.setLayout(new PatternLayout(PatternLayout.TTCC_CONVERSION_PATTERN));
+        this.logger.addAppender(consoleAppender);
     }
 
     Runnable periodicallyGetKafkaTopics = new Runnable() {
@@ -62,12 +57,14 @@ public class Bridge implements MqttCallback {
             logger.info("Started Thread to periodically check and subscribe to kafka topics");
             while (true) {
                 try {
-                    List<String> topics = notificationService.getTopics().execute().body();
-                    if (topics != null && topics.size() > 0) {
-                        subcribeToTopics(topics.toArray(new String[topics.size()]));
+                    String[] topics = kafkaAdmin.listTopics().names().get().toArray(String[]::new);
+                    logger.log(Level.INFO, "Got topics");
+                    if (topics != null && topics.length > 0) {
+                        logger.log(Level.INFO, "Topics not null and more than one");
+                        subcribeToTopics(topics);
                     }
-                    Thread.sleep(fiveMinutes);
-                    logger.trace("Thread going to sleep");
+                    logger.trace("Thread to check new Topics going to sleep");
+                    Thread.sleep(oneMinute);
                 } catch (Exception e) {
                     logger.error(e.getMessage());
                 }
@@ -85,11 +82,11 @@ public class Bridge implements MqttCallback {
             IMqttToken token = mqtt.connect(connOpts);
             Properties props = new Properties();
             props.put("bootstrap.servers", brokerUri);
+            kafkaAdmin = KafkaAdminClient.create(props);
             props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
             props.put("value.serializer", "org.springframework.kafka.support.serializer.JsonSerializer");
             kafkaProducer = new KafkaProducer<>(props);
             token.waitForCompletion();
-            mqtt.subscribe("test", 0);
         } catch (MqttException e) {
             e.printStackTrace();
         }
@@ -125,7 +122,7 @@ public class Bridge implements MqttCallback {
         byte[] payload = message.getPayload();
         System.out.println("NEW MQTT MESSAGE - " + new String(payload));
         if (!topic.contains("cmd")) {
-            Notification notification = new Notification(topic, new String(payload));
+            Notification notification = new Notification(new String(payload), topic);
             kafkaProducer.send((new ProducerRecord<>(topic, notification)));
         }
     }
@@ -150,17 +147,14 @@ public class Bridge implements MqttCallback {
     }
 
     public static ResponseEntity<String> testForHealth() {
-        if (notificationService == null)
-            return new ResponseEntity<>("Notification Service for Retrofit is null", HttpStatus.EXPECTATION_FAILED);
-        else if (mqtt == null || kafkaProducer == null)
+        if (mqtt == null || kafkaProducer == null || kafkaAdmin == null)
             return new ResponseEntity<>("Kafka or Mqtt is null", HttpStatus.INTERNAL_SERVER_ERROR);
         try {
             mqtt.checkPing(null, null);
+            kafkaProducer.metrics();
+            kafkaAdmin.describeCluster();
         } catch (MqttException e) {
             return new ResponseEntity<>("Mqtt Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        try {
-            kafkaProducer.metrics();
         } catch (Exception e) {
             return new ResponseEntity<>("Kafka Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
         }
