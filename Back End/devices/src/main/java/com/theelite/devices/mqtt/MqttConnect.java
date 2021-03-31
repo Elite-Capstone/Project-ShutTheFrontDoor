8111e6f9bf9bc78992cb3de41bb2f64862f42652
Dean <deancs1997@gmail.com>
2021-03-31 14:06:40 -0400

    can now subscribe to mqtt topics
    So can now retrieve messages from mqtt

--- Back End/devices/src/main/java/com/theelite/devices/mqtt/MqttConnect.java
+++ Back End/devices/src/main/java/com/theelite/devices/mqtt/MqttConnect.java
@@ -2,25 +2,32 @@ package com.theelite.devices.mqtt;
 
 import com.theelite.devices.service.DeviceServiceImpl;
 import org.eclipse.paho.client.mqttv3.*;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class MqttConnect implements MqttCallback {
     private String mqttServerUrl;
+    public final int fiveMinutes = 300000;
     public static MqttAsyncClient mqttClient;
     private DeviceServiceImpl deviceService;
 
     public MqttConnect(Environment environment, DeviceServiceImpl deviceService) {
-        this.mqttServerUrl = environment.getProperty("mqtt.server");
+        mqttServerUrl = environment.getProperty("mqtt.server");
         this.deviceService = deviceService;
+        connectToMqttBroker();
+        periodicallySubscribeToTopics.run();
     }
 
 
     private void connectToMqttBroker() {
         try {
-            mqttClient = new MqttAsyncClient(this.mqttServerUrl, MqttAsyncClient.generateClientId());
+            String clientId = MqttAsyncClient.generateClientId();
+            mqttClient = new MqttAsyncClient(mqttServerUrl, clientId);
             mqttClient.setCallback(this);
             MqttConnectOptions connOpts = new MqttConnectOptions();
             connOpts.setCleanSession(true);
@@ -29,7 +36,6 @@ public class MqttConnect implements MqttCallback {
         } catch (MqttException e) {
             e.printStackTrace();
         }
-
     }
 
     private void reconnectToMqttBroker() throws MqttException {
@@ -68,5 +74,42 @@ public class MqttConnect implements MqttCallback {
         } catch (MqttException e) {
             e.printStackTrace();
         }
+    }
+
+
+    Runnable periodicallySubscribeToTopics = new Runnable() {
+        @Override
+        public void run() {
+//            logger.info("Started Thread to periodically check and subscribe to kafka topics");
+            while (true) {
+                try {
+                    var deviceIds = deviceService.getDeviceIds();
+//                    logger.log(Level.INFO, "Got topics");
+                    if (deviceIds != null && deviceIds.size() > 0) {
+//                        logger.log(Level.INFO, "Topics not null and more than one");
+                        subscribeToTopics(getMqttTopicsFromDeviceIds(deviceIds).toArray(String[]::new));
+                    }
+//                    logger.trace("Thread to check new Topics going to sleep");
+                    Thread.sleep(fiveMinutes);
+                } catch (Exception e) {
+//                    logger.error(e.getMessage());
+                }
+            }
+        }
+    };
+
+    private void subscribeToTopics(String[] topics) throws MqttException {
+        int[] qos = new int[topics.length];
+        Arrays.stream(qos).forEach(q -> q = 0);
+        mqttClient.subscribe(topics, qos);
+    }
+
+    private List<String> getMqttTopicsFromDeviceIds(List<String> deviceIds) {
+        var topics = new ArrayList<String>();
+        deviceIds.forEach(id -> {
+            topics.add("command/" + id);
+            topics.add("status/" + id);
+        });
+        return topics;
     }
 }
